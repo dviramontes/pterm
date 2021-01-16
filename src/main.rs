a3873f9de6a9950db2300d4a4ddea5fee32ce4bf
dviramontes <dviramontes@gmail.com>
2021-01-16 10:26:09 -0700

    feat: pgrep -l

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
+use regex::Regex;
 use sysinfo::{ProcessExt, SystemExt};
 
-// TODO: find process in list
 // TODO: send KILLSIG to process or list of processes
 
 fn main() {
-    let pname = std::env::args().nth(1).expect("no process name given");
-    println!("{}", pname);
+    let proc_name = std::env::args().nth(1).expect("no process name given");
+    println!("looking for : {}", proc_name);
 
     let mut system = sysinfo::System::new_all();
-
+    let matcher = format!(r"{}$", proc_name);
+    let re = Regex::new(&matcher).unwrap();
     system.refresh_all();
 
-    for (pid, proc_) in system.get_processes() {
+    for (pid_, proc_) in system.get_processes() {
         // TODO: add guard for process status
-        println!("{}:{} => status {:?}", pid, proc_.name(), proc_.status());
+        if re.is_match(&proc_.name()) {
+            println!("{}:{} => status {:?}", pid_, proc_.name(), proc_.status())
+        }
     }
 }
