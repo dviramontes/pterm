1618709c10bfdf3c26804fe4ea74124a951dc61e
dviramontes <dviramontes@gmail.com>
2021-01-16 12:40:27 -0700

    send sigterm, add readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# `ifkill`
+
+> Kills a process by name when found. Think of it as a diet coke version of `pgrep Python | xargs kill -SIGTERM` 
+
+### Development
+
+- `cargo run <process_name>`
+
+#### Requirements
+
+- [rust](https://www.rust-lang.org/)
+
+#### Building
+
+- `cargo build` # debug version
+- `cargo build --release` # final version
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
+use nix::sys::signal::{self, Signal};
+use nix::unistd::Pid;
 use regex::Regex;
 use sysinfo::{ProcessExt, SystemExt};
 
-// TODO: send KILLSIG to process or list of processes
-
 fn main() {
     let proc_name = std::env::args().nth(1).expect("no process name given");
-    println!("looking for : {}", proc_name);
-
     let mut system = sysinfo::System::new_all();
-    let matcher = format!(r"{}$", proc_name);
+    let matcher = format!(r"(?i){}$", proc_name);
     let re = Regex::new(&matcher).unwrap();
+
     system.refresh_all();
 
-    for (pid_, proc_) in system.get_processes() {
-        // TODO: add guard for process status
-        if re.is_match(&proc_.name()) {
-            println!("{}:{} => status {:?}", pid_, proc_.name(), proc_.status())
+    for (pid, proc) in system.get_processes() {
+        if re.is_match(&proc.name()) {            
+            match proc.status() {
+                sysinfo::ProcessStatus::Run => {
+                    signal::kill(Pid::from_raw(*pid), Signal::SIGTERM).unwrap();
+                    println!("{} {} SIGTERM", pid, proc.name())
+                }
+                _ => println!("process: {} not currently running", proc_name),
+            }
         }
     }
 }
